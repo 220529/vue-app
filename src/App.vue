0f035106e10f10f4dfc3951510d8178d996a2cdf
liukaixin <15713857919@163.com>
2024-10-22 18:52:32 +0800

    add: vue-lazyload

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
-import Counter from "@/components/Counter.vue";
-import ListComponent from "@/components/ListComponent.vue";
+// import Counter from "@/components/Counter.vue";
+// import ListComponent from "@/components/ListComponent.vue";
+import LazyLoadGallery from "@/components/LazyLoadGallery.vue";
 </script>
 
 <template>
-  <Counter msg="Vite" />
-  <ListComponent />
+  <!-- <Counter msg="Vite" /> -->
+  <!-- <ListComponent /> -->
+  <LazyLoadGallery />
 </template>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
 import { createApp } from "vue";
+import VueLazyload from "vue-lazyload";
+
 import "./reset.css";
 import "./style.css";
 import App from "./App.vue";
@@ -10,6 +12,13 @@ const app = createApp(App);
 // 全局注册指令
 Object.entries(directives).forEach(([name, directive]) => {
   app.directive(name.replace("v-", ""), directive);
+});
+
+app.use(VueLazyload, {
+  preLoad: 1.3, // 提前加载高度（视口高度的比例）
+  error: "https://lagou-zhaopin-fe.lagou.com/activities/imgs/6.webp",
+  loading: "https://lagou-zhaopin-fe.lagou.com/activities/imgs/loading.gif",
+  attempt: 1, // 尝试加载的次数
 });
 
 app.mount("#app");
