654a78a735e7bbae76cbef01d5e49ab069ff6e1b
liukaixin <15713857919@163.com>
2025-03-19 15:44:25 +0800

    univer

--- src/components/univer/index.vue
+++ src/components/univer/index.vue
@@ -5,17 +5,17 @@
 <script setup>
 import { createUniver, defaultTheme, LocaleType, merge } from '@univerjs/presets'
 import { UniverSheetsConditionalFormattingPreset } from '@univerjs/presets/preset-sheets-conditional-formatting'
-import sheetsConditionalFormattingEnUS from '@univerjs/presets/preset-sheets-conditional-formatting/locales/en-US'
+import sheetsConditionalFormattingZhCN from '@univerjs/presets/preset-sheets-conditional-formatting/locales/zh-CN'
 import { UniverSheetsCorePreset } from '@univerjs/presets/preset-sheets-core'
-import sheetsCoreEnUS from '@univerjs/presets/preset-sheets-core/locales/en-US'
+import sheetsCoreZhCN from '@univerjs/presets/preset-sheets-core/locales/zh-CN'
 import { UniverSheetsDataValidationPreset } from '@univerjs/presets/preset-sheets-data-validation'
-import sheetsDataValidationEnUS from '@univerjs/presets/preset-sheets-data-validation/locales/en-US'
+import sheetsDataValidationZhCN from '@univerjs/presets/preset-sheets-data-validation/locales/zh-CN'
 import { UniverSheetsDrawingPreset } from '@univerjs/presets/preset-sheets-drawing'
-import sheetsDrawingEnUS from '@univerjs/presets/preset-sheets-drawing/locales/en-US'
+import sheetsDrawingZhCN from '@univerjs/presets/preset-sheets-drawing/locales/zh-CN'
 import { UniverSheetsFilterPreset } from '@univerjs/presets/preset-sheets-filter'
-import sheetsFilterEnUS from '@univerjs/presets/preset-sheets-filter/locales/en-US'
+import sheetsFilterZhCN from '@univerjs/presets/preset-sheets-filter/locales/zh-CN'
 import { UniverSheetsHyperLinkPreset } from '@univerjs/presets/preset-sheets-hyper-link'
-import sheetsHyperLinkEnUS from '@univerjs/presets/preset-sheets-hyper-link/locales/en-US'
+import sheetsHyperLinkZhCN from '@univerjs/presets/preset-sheets-hyper-link/locales/zh-CN'
 import { WORKBOOK_DATA } from './data'
 
 import './style.css'
@@ -27,16 +27,16 @@ import '@univerjs/presets/lib/styles/preset-sheets-filter.css'
 import '@univerjs/presets/lib/styles/preset-sheets-hyper-link.css'
 
 const { univerAPI } = createUniver({
-    locale: LocaleType.EN_US,
+    locale: LocaleType.ZH_CN,
     locales: {
-        [LocaleType.EN_US]: merge(
+        [LocaleType.ZH_CN]: merge(
             {},
-            sheetsCoreEnUS,
-            sheetsConditionalFormattingEnUS,
-            sheetsDataValidationEnUS,
-            sheetsDrawingEnUS,
-            sheetsFilterEnUS,
-            sheetsHyperLinkEnUS,
+            sheetsCoreZhCN,
+            sheetsConditionalFormattingZhCN,
+            sheetsDataValidationZhCN,
+            sheetsDrawingZhCN,
+            sheetsFilterZhCN,
+            sheetsHyperLinkZhCN,
         ),
     },
     theme: defaultTheme,
